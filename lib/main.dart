e068068f0326a2b3bf8a056b1224ef6d2c5d3df1
Atri Das <babinsamrat123@gmail.com>
2020-04-04 18:24:45 +0530

    makes it simple, after that will do more implementations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/screen/business_news_screen.dart';
 
-main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(
-      primaryColor: Colors.blueGrey[900],
-  ),
-  home: BusinessNewsScreen(),
-));
+main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.blueGrey[900],
+        ),
+        home: BusinessNewsScreen(),
+      ),
+    );
--- lib/resources/api_url.dart
+++
@@ -1,3 +0,0 @@
-class ApiUrl{
-  String businessNewsUrl = 'https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=3fc464c420834d6fb76899f176e1ae6d';
-}
