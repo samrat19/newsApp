f87a6f52826402fe65b2595782d3609d7bfa07a5
Atri Das <babinsamrat123@gmail.com>
2020-04-05 09:09:33 +0530

    adds Stream Builder

--- lib/screen/business_news_screen.dart
+++ lib/screen/business_news_screen.dart
@@ -1,39 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/logic/model/news_response_model.dart';
+import 'package:newsapp/logic/news_bloc.dart';
 
 class BusinessNewsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    newsBloc.fetchLondonWeather();
+
     return Scaffold(
-      body: Container(
-          color: Colors.blueGrey[900],
-          child: ListView.builder(
-            itemCount: 1,
-            itemBuilder: (BuildContext context, int index) {
-              return Column(
-                children: <Widget>[
-                  //todo
-                  Image.network(''),
-                  Divider(),
-                  ClipRRect(
-                    clipBehavior: Clip.hardEdge,
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: Card(
-                      color: Colors.blueGrey[800],
-                      //todo
-                      child: Text(
-                        '',
-                        style: TextStyle(fontSize: 20.0, color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  Divider(
-                    height: 20.0,
-                  ),
-                ],
+      appBar: AppBar(),
+      body: StreamBuilder(
+          stream: newsBloc.news,
+          builder: (context, AsyncSnapshot<NewsResponse> snapshot) {
+            if (snapshot.hasData) {
+              return Container(
+                color: Colors.blueGrey[900],
+                child: ListView.builder(
+                  itemCount: 1,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: <Widget>[
+                        //todo
+                        Image.network(snapshot.data.urlToImage),
+                        Divider(),
+                        ClipRRect(
+                          clipBehavior: Clip.hardEdge,
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Card(
+                            color: Colors.blueGrey[800],
+                            //todo
+                            child: Text(
+                              snapshot.data.title,
+                              style: TextStyle(
+                                  fontSize: 20.0, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        Divider(
+                          height: 20.0,
+                        ),
+                      ],
+                    );
+                  },
+                ),
               );
-            },
-          )),
+            } else {
+              return CircularProgressIndicator();
+            }
+          }),
     );
-    ;
   }
 }
