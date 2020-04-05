53081ea7fe671292ddd4a2cb033b775a496dd499
Atri Das <babinsamrat123@gmail.com>
2020-04-05 09:00:15 +0530

    adds scaffold

--- lib/screen/business_news_screen.dart
+++ lib/screen/business_news_screen.dart
@@ -3,31 +3,37 @@ import 'package:flutter/material.dart';
 class BusinessNewsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey[900],
-      child: ListView.builder(
-        itemCount: 1,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: <Widget>[
-              //todo
-              Image.network(''),
-              Divider(),
-              ClipRRect(
-                clipBehavior: Clip.hardEdge,
-                borderRadius: BorderRadius.circular(20.0),
-                child: Card(
-                  color: Colors.blueGrey[800],
+    return Scaffold(
+      body: Container(
+          color: Colors.blueGrey[900],
+          child: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
                   //todo
-                  child: Text('',style: TextStyle(fontSize: 20.0,
-                      color: Colors.white),),
-                ),
-              ),
-              Divider(height: 20.0,),
-            ],
-          );
-        },
-      )
-    );;
+                  Image.network(''),
+                  Divider(),
+                  ClipRRect(
+                    clipBehavior: Clip.hardEdge,
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Card(
+                      color: Colors.blueGrey[800],
+                      //todo
+                      child: Text(
+                        '',
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    height: 20.0,
+                  ),
+                ],
+              );
+            },
+          )),
+    );
+    ;
   }
 }
